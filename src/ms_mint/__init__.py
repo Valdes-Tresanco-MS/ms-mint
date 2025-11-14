63bdc4b17bacac63f5428d5f982e1b1e72b23cd0
Mario S. Valdés <mariosergiovaldes145@gmail.com>
2025-11-14 17:42:13 -0500

    refactor version management to use versioneer for dynamic versioning

--- src/ms_mint/__init__.py
+++ src/ms_mint/__init__.py
@@ -1,12 +1,17 @@
 import os
 import logging
 from .Mint import Mint
-from ._version import __version__
+from . import _version
 
 __all__ = ["__version__"]
 
 MINT_DATA_PATH = os.path.abspath(os.path.join(__path__[0], "..", "static"))
 
+__version__ = _version.get_versions()['version']
+
 Mint.version = __version__
 
 logging.info(Mint.version)
+
+
+
